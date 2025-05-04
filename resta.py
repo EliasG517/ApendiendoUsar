c33eb965ab30129b67846addcfa22fb8fb301ac8
EliasG517 <eliasnicolas.gonzalez2002@gmail.com>
2025-05-04 06:03:32 -0400

    Funcion de resta

---
+++ resta.py
@@ -0,0 +1,8 @@
+def restar_numeros():
+    numero1 = int(input("Ingresa el primer número: "))
+    numero2 = int(input("Ingresa el segundo número: "))
+    resultado = numero1 - numero2
+    print("El resultado de la resta es:", resultado)
+
+# Llamada a la función
+restar_numeros()
